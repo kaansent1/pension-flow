bfc61e9c0ed74b3df9da8c9541ec3171bf952547
Kaan Sentürk <kaan.sentuerk@stud.hs-flensburg.de>
2026-08-11 23:38:10 +0200

    Initial project setup

---
+++ services/process-service/src/test/java/de/kaan/pensionflow/processservice/ProcessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kaan.pensionflow.processservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
